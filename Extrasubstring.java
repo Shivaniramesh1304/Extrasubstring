648b974adfdaf543ac0b9658c2c6859dcd9fdb05
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:36:34 +0530

    Add files via upload

---
+++ Extrasubstring.java
@@ -0,0 +1,11 @@
+public class Extrasubstring {
+    public static void main(String[] args) {
+        String mainString="hello, world!";
+        int startindex=7;
+        int endindex=12;
+        String substring=mainString.substring(startindex,endindex);
+        System.out.println("extracted substring:" + substring);
+    }
+ }
+    
+
